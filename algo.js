67a7768db035c288d5aefc2df435e0b452cff50f
AhmadAbdelwahab1 <74326212+AhmadAbdelwahab1@users.noreply.github.com>
2021-08-21 12:15:38 +0200

    Add files via upload

--- algo.js
+++ algo.js
@@ -1,59 +1,73 @@
 
 var sites = [
     {
-        "name": "Front_end of Hospital Website"
+        "name": "  Hospital Website"
         ,
         "imgSrc":"hospital1.jpg"
         ,
         "example":"https://www.jbl.de/en/online-hospital?country=bg"
+        ,
+        "color" : "rgba(100, 105, 151,0.8)"
      }
     ,
     {
-        "name": "Front_end of Gym Website"
+        "name": "  Gym Website"
         ,
         "imgSrc":"gym1.jpg"
         ,
         "example":"https://www.smart-gym.com/"
+        ,
+        "color":"rgba(71, 39, 88,0.8)"
      }
      ,
      {
-        "name": "Front_end of Online_shop Website"
+        "name": "  Online shop Website"
         ,
         "imgSrc":"online-shop1.jpg"
         ,
         "example":"https://www.ubuy.com.eg/en/"
+        ,
+        "color":"rgba(5, 55, 147,0.7)"
      }
      ,
      {
-         "name": "Front_end of University Website"
+         "name": "  University Website"
          ,
          "imgSrc":"school1.jpg"
          ,
          "example":"https://www.harvard.edu/"
+         ,
+         "color":"rgba(5, 56, 147,0.7)"
      }
      ,
      {
-         "name": "Front_end of Order_Coffe Website"
+         "name": "  Order Coffe Website"
          ,
          "imgSrc":"coffe.jpg"
         ,
         "example":"https://www.drivencoffee.com/"
+        ,
+        "color":"rgba(250, 222, 198,0.7)"
      }
      ,
      {
-         "name": "Front_end of Banking Website"
+         "name": "  Banking Website"
          ,
          "imgSrc":"banking.jpg"
         ,
         "example":"https://www.nbe.com.eg/NBE/E/#/EN/Home"
+        ,
+        "color":"rgba(92, 62, 171,0.7)"
      }
      ,
      {
-         "name": "Front_end of Booking_Travil Website"
+         "name": "  Booking Travil Website"
          ,
          "imgSrc":"booking.jpg"
         ,
         "example":"https://ar.trivago.com/en?sLanguageLocale=UK"
+        ,
+        "color":"rgba(138, 1, 147,0.7)"
      }
      
      
@@ -77,8 +91,11 @@ function GenrateRandomSite()
 
     
     document.getElementById("siteimg").src= "giphy.gif";
+    document.getElementById("site").innerHTML = "Random site";
+
       
     setTimeout(() => {
+        document.getElementById('randomContainer').style.backgroundColor = sites[rand].color ; 
     document.getElementById("site").innerHTML = sites[rand].name;
     document.getElementById("sitea").href=sites[rand].example; 
     document.getElementById("siteimg").src=sites[rand].imgSrc;
